a8c53035172bdd975d0b66389752b79b081ddec8
Vladislav Semyonoff <vsemyonoff@gmail.com>
2020-04-25 18:17:28 +0300

    cmdd::cmake: update project version

--- config.cmake
+++ config.cmake
@@ -8,7 +8,7 @@ set (HOMEPAGE     "https://github.com/vsemyonoff/cmdd")
 # Version
 set (VERSION_MAJOR 1)
 set (VERSION_MINOR 0)
-set (VERSION_PATCH 0)
+set (VERSION_PATCH 1)
 
 # C++ standard version
 set (REQUIRED_CXX_STANDARD 17)
